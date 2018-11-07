85f9ab46115d4bc7b1edb4e29b628d995c68ed22
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    sm8250-common: Remove obsolete dex settings
    This block is completely redundant because..
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY is set
      in vendor/lineage.
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,13 +111,6 @@ USE_XML_AUDIO_POLICY_CONF := 1
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
-  endif
-endif
-
 # Display
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 
