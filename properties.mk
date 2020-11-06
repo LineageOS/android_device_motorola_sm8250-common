acdb539310a85ef6474190a0a1d8c2f02d3ebc36
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2020-11-06 17:03:11 +0530

    sm8250-common: Add default values for SurfaceFlinger
    This change sets the default values for the maximum number of buffers
    SF would allocate for use in FramebufferSurface, and the maximum
    dimension supported by HWC for virtual display
    
    Change-Id: I29ce6f0adb69473e8d99d2a5e506b312f25b2475
    CRs-Fixed: 2778423

--- properties.mk
+++ properties.mk
@@ -373,6 +373,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.wcg_composition_dataspa
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_virtual_display_dimension=4096
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.idle_time=0 \
