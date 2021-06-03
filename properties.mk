973b879857de8c82631bae95a13273186587c789
Lucas Dupin <dupin@google.com>
2021-06-03 16:45:21 -0700

    sm8250-common: Enable blurs
    Bug: 181092919
    Test: manual; performance scripts
    Change-Id: I5875a99167e80ddd5b79fc245f988d8a6ac167be

--- properties.mk
+++ properties.mk
@@ -376,6 +376,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_virtual_display_dimension=4096
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.supports_background_blur=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.idle_time=0 \
