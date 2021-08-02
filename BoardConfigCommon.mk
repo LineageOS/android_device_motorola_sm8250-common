96940d8fc3e86552e0d7a784de7a21674b37adef
LuK1337 <priv.luk@gmail.com>
2021-08-02 22:47:55 +0200

    sm8250-common: Include device FCM from vendor/lineage
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,7 +143,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
