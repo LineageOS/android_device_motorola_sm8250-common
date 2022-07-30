967ff87bef6623f485a7788749e2631fbbc3abf8
electimon <electimon@gmail.com>
2022-07-30 03:31:11 +0000

    sm8250-common: Decommonize NFC specifics
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I14c3cb0fad87140ef0e134284b0a7d09ecfc3b31

--- common.mk
+++ common.mk
@@ -400,15 +400,10 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
     Tag
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
 PRODUCT_PACKAGES += \
