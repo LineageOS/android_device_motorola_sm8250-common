213ddf7cd3cd556d24816f83ee970a2722685f02
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    sm8250-common: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,7 +206,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-06-01
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
