4709d252f6d417655aa80661d988ee8001999e11
dianlujitao <dianlujitao@lineageos.org>
2022-10-03 17:13:04 +0800

    sm8250-common: Remove kernel flags now populated by default
    Change-Id: I8018945410077d0944b49d780a815595efefd9d4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,13 +46,8 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 an
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
-    LLVM=1
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/sm8250
-TARGET_KERNEL_VERSION := 4.19
-TARGET_KERNEL_CLANG_COMPILE := true
 KERNEL_LTO := none
 
 # Kernel modules - Audio
