198f222ae6570d11c1804e83b4d2b3ed2ad56c64
dianlujitao <dianlujitao@lineageos.org>
2022-10-07 20:29:49 +0800

    sm8250-common: Nuke ATFWD
    Moto starts the service in bp-tools and qcom modes only, so it's
    normally not used at all.
    
    Change-Id: Idb824b3b8c783b6ac962e604df05f9154beb4ed2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,7 +135,6 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
