1fea05509b0dc264d40f0e807ca4bb939bd1a8c7
dianlujitao <dianlujitao@lineageos.org>
2022-10-07 13:20:24 +0800

    sm8250-common: Drop init.mmi.block_perm.sh
    Doesn't seem to be useful, and it never ever worked due to missing
    sepolicy anyway.
    
    Change-Id: I2dd50a186b5fbe2b230003363ff4ef152be31343

--- common.mk
+++ common.mk
@@ -224,7 +224,6 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.ets.rc \
     init.mdm.sh \
-    init.mmi.block_perm.sh \
     init.mmi.charge_only.rc \
     init.mmi.laser.sh \
     init.mmi.overlay.rc \
