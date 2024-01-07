2007d955d5b6fc7bee1645b4ee06c826fefe44f8
dianlujitao <dianlujitao@lineageos.org>
2024-01-07 14:15:46 +0800

    sm8250-common: Rename camera.mot.is.coming.cts for com.qti.chi.override.so
    Also clean up pattern matching while at it.
    
    Change-Id: I3733d7ad32b8f639e2509cc9926736fdaf302029

--- extract-files.sh
+++ extract-files.sh
@@ -71,20 +71,14 @@ function blob_fixup_common() {
         system_ext/etc/permissions/moto-telephony.xml)
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
-        vendor/bin/thermal-engine)
-            sed -i 's/ro.mot.build.customerid/vendor.build.customerid/g' "${2}"
-            ;;
-        vendor/bin/rmt_storage)
+        vendor/bin/thermal-engine|vendor/bin/rmt_storage|vendor/lib64/libril-qc-hal-qmi.so)
             sed -i 's/ro.mot.build.customerid/vendor.build.customerid/g' "${2}"
             ;;
         vendor/etc/init/vendor.qti.hardware.alarm@1.0-service.rc)
             sed -i "/disabled/d" "${2}"
             ;;
-        vendor/lib64/hw/camera.qcom.so)
+        vendor/lib64/hw/camera.qcom.so|vendor/lib64/hw/com.qti.chi.override.so)
             sed -i "s/camera.mot.is.coming.cts/vendor.camera.coming.cts/g" "${2}"
-            ;;
-        vendor/lib64/libril-qc-hal-qmi.so)
-            sed -i 's/ro.mot.build.customerid/vendor.build.customerid/g' "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/130A0094/1F2003D5/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
