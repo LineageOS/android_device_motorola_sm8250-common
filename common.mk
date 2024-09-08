a70c71c4caf894c4a1846135e8fc0c011a27b765
dianlujitao <dianlujitao@lineageos.org>
2024-09-08 22:54:15 +0800

    sm8250-common: move product inheritance from device
    Change-Id: I51b72ea94d63a4c77c1ca990896748ed0caf05f6

--- common.mk
+++ common.mk
@@ -14,6 +14,16 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 30
 
