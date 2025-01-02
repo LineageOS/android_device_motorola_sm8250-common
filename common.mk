1b21fc2309eec5c10349f11dcd9f34bc91212734
dianlujitao <dianlujitao@lineageos.org>
2025-01-02 11:34:02 +0800

    sm8250-common: move to aidl bootctrl hal
    Change-Id: I50fd09148097f0cda03c1a105340a3fd5f55ce8d

--- common.mk
+++ common.mk
@@ -192,9 +192,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Bluetooth
 PRODUCT_PACKAGES += \
