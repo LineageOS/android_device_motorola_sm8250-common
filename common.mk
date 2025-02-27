913879354a18ad9979a3a36abf39c6d2597e6903
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    sm8250-common: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- common.mk
+++ common.mk
@@ -328,6 +328,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+OVERRIDE_ENABLE_UFFD_GC := true
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
