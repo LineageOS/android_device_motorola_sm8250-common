2a59fa2afa364a65659a31b1a194e26bee066fa2
basamaryan <basam.aryan@gmail.com>
2025-03-17 01:25:44 -0400

    sm8250-common: Use hardware/motorola/sepolicy
    Change-Id: I3db249a436d258867d431b6a0e837d9053b85ddd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,9 +191,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include hardware/motorola/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
