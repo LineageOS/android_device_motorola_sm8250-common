331eee2c7e80f17f1b4a690f4d8cb8f39f7229c6
Nolen Johnson <johnsonnolen@gmail.com>
2025-06-29 16:31:53 -0400

    sm8250-common: sepolicy: Address remaining denials
    * Reformat file_contexts while here.
    
    Change-Id: I9b9df7c8e9ff4a9ad893b63f8bb2d3e1aa0246cc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,6 +193,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
