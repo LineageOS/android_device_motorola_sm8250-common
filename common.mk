4a446d51014eb234993d89df9cc09d859e9c2849
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-09 13:12:12 -0500

    fixup! sm8250-common: Disable `thumbnail_block_model` of libstagefright
    Change-Id: If5bb6276c79c4e135c95fd5bf114ddb60a2cd963

--- common.mk
+++ common.mk
@@ -356,7 +356,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/video_system_specs.json
 
-$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 # Moto hardware
 PRODUCT_PACKAGES += \
