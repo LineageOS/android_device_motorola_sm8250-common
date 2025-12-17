ba4370834ef170378e59b5629f5558d4bd54742d
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-17 00:59:43 -0500

    Revert "sm8250-common Use clang r530567"
    This reverts commit 80621f9fd9fa0c32b30a5103630ecc0778e228d4.
    
    Reason: Works on common kernel.
    Change-Id: I3bac7a1c1989ac3c154dafe40514fbe9c61173f0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,6 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CLANG_VERSION := r530567
 TARGET_KERNEL_CONFIG := \
     vendor/kona-perf_defconfig \
     vendor/ext_config/moto-kona.config
