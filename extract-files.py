bfb550e3eb7f4c3036e7f1d9f1b7a57b1eb0bdf4
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-17 21:25:56 +0000

    Revert "sm8250-common: Patch some blobs to depend on libtinyxml2_1.so"
    This reverts commit cb713bba20392ab0d0dc234ed51efa77972c7f71.
    
    Reason for revert: FF mispush, will Reland it.
    
    Change-Id: I73b641e4429a67d4c95fce43875229521e49925c

--- extract-files.py
+++ extract-files.py
@@ -53,8 +53,6 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'ro.mot.build.customerid', b'vendor.build.customerid'),
     'vendor/etc/init/vendor.qti.hardware.alarm@1.0-service.rc': blob_fixup()
         .regex_replace('    disabled', ''),
-    'vendor/lib64/libdpps.so': blob_fixup()
-        .replace_needed('libtinyxml2.so', 'libtinyxml2_1.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
