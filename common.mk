46a427d6f06c187ff70b2942749e1e7d77e600d4
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-20 00:56:47 -0500

    sm8250-common: properties.mk -> $partition.prop
    * Sort and alphabetize while here.
    
    Change-Id: I1bdba491285b04189def02b1bfbf89f6bde7964b

--- common.mk
+++ common.mk
@@ -84,9 +84,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Properties
-$(call inherit-product, $(LOCAL_PATH)/properties.mk)
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
