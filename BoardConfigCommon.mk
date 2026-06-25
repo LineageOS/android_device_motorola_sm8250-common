d4525dfaf5fa924e9a69a3c0c7e4a8b746ff0705
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:07:21 -0400

    sm8250-common: Use Clang r563880c for kernel build
    * Doesn't boot with default Clang version.
    
    Change-Id: I35765e296044eef428d01bbf5a21e91f1897704e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_XZ := true
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_CONFIG := \
     vendor/kona-perf_defconfig \
     vendor/ext_config/moto-kona.config
